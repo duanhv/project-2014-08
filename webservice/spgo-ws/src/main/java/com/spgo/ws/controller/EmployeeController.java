0a416e662598bae0a46075c34399183125cce761
duanhv <duanhv@gmail.com>
2014-09-27 21:19:13 +0000

    get json

---
+++ webservice/spgo-ws/src/main/java/com/spgo/ws/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.spgo.ws.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.spgo.ws.business.EmployeeManager;
+import com.spgo.ws.domain.info.EmployeeInfo;
+
+@Controller
+@RequestMapping("/service/employee/")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeManager employeeManager;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET ,headers="Accept=application/json")
+	@ResponseBody
+	public EmployeeInfo getEmployee(@PathVariable String id) {
+
+		
+		EmployeeInfo info = null;
+		try {
+			info = employeeManager.getEmployeeByLoginId(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return info;
+	}
+	
+}
