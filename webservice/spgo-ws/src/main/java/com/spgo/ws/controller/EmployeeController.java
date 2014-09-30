06bf15a3415d1921d12e4d8237e1a56ba9d69444
dung.phan88ht@gmail.com <dung.phan88ht@gmail.com@df858c5c-a441-d367-c21b-d2b714e6a04f>
2014-09-30 17:37:42 +0000

    dung phan

--- webservice/spgo-ws/src/main/java/com/spgo/ws/controller/EmployeeController.java
+++ webservice/spgo-ws/src/main/java/com/spgo/ws/controller/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.spgo.ws.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.spgo.ws.business.EmployeeManager;
+import com.spgo.ws.dao.EmployeeDao;
+import com.spgo.ws.domain.bean.EmployeeModel;
 import com.spgo.ws.domain.info.EmployeeInfo;
 
 @Controller
@@ -16,6 +21,8 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeManager employeeManager;
+	@Autowired
+	private EmployeeDao employeeDao;
 	
 	@RequestMapping(value = "/getInfo", method = RequestMethod.GET ,headers="Accept=application/json")
 	@ResponseBody
@@ -30,6 +37,16 @@ public class EmployeeController {
 		}
 		
 		return info;
+	}
+	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST ,headers="Accept=application/json")
+	 @ResponseStatus( HttpStatus.CREATED )
+	public void addEmployeeFromJson(@RequestBody final EmployeeModel employeeModel){		
+		try{
+			employeeDao.addEmployee(employeeModel);
+		} catch(Exception ex){
+			System.out.println("Can not save new employee");
+		}
+		
 	}
 	
 }
--- webservice/spgo-ws/src/main/java/com/spgo/ws/controller/MyResource.java
+++ webservice/spgo-ws/src/main/java/com/spgo/ws/controller/MyResource.java
@@ -3,11 +3,22 @@ package com.spgo.ws.controller;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spgo.ws.dao.EmployeeDao;
+import com.spgo.ws.domain.bean.EmployeeModel;
+import com.spgo.ws.oauth.security.CustomUserAuthenticationProvider;
+
 
 @Path("/MyResource")
 public class MyResource {
-	
-	
+	@Autowired
+	private EmployeeDao employeeDao;
 	@GET
 	@Path("/createInfo")
 	public String createInfo(){
@@ -15,11 +26,22 @@ public class MyResource {
 
 	}
 	
-	@GET
-	@Path("/getMyInfo")
-	public String getMyInfo(){
+	@RequestMapping(value = "/getMyInfo", method = RequestMethod.GET,headers="Accept=application/json")
+	public EmployeeModel getMyInfo(){
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication != null && StringUtils.isNotBlank((String)authentication.getPrincipal())){
+			String email = (String)authentication.getPrincipal();
+			EmployeeModel employee = employeeDao.getEmployeeByLoginId(email);
+			if(employee == null ){
+				return null;
+			} else {
+				return employee;
+			}			
+		} else {
+			return null;
+		}
 		
-		return "\n\n\t Protected Resource(getMyInfo) Accessed !!!! Returning from Myresource getMyInfo\n";
+		
 	}
 	
 	
