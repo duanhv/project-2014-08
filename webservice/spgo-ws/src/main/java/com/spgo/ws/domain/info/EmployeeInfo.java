7b60bbc193a9a21839bac10f4da0f002055234a3
duanhv <duanhv@gmail.com>
2014-10-01 09:04:43 +0000

    Add add Employee to Manager

--- webservice/spgo-ws/src/main/java/com/spgo/ws/domain/info/EmployeeInfo.java
+++ webservice/spgo-ws/src/main/java/com/spgo/ws/domain/info/EmployeeInfo.java
@@ -3,16 +3,22 @@ package com.spgo.ws.domain.info;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.ser.std.DateSerializer;
+
 public class EmployeeInfo implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private String id;
 	private String name;
-	private String loginId;
 	private String email;
 	private String password;
 	private Date birthDay;
+	private String phone;
+	private int age;
+	private String gender;
+	
 	private String updatedBy;
 	private Date updatedDate;
 	private String createdBy;
@@ -21,79 +27,74 @@ public class EmployeeInfo implements Serializable {
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getLoginId() {
-		return loginId;
-	}
-
-	public void setLoginId(String loginId) {
-		this.loginId = loginId;
-	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	@JsonSerialize(using=DateSerializer.class)
 	public Date getBirthDay() {
 		return birthDay;
 	}
-
 	public void setBirthDay(Date birthDay) {
 		this.birthDay = birthDay;
 	}
-
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 	public String getUpdatedBy() {
 		return updatedBy;
 	}
-
 	public void setUpdatedBy(String updatedBy) {
 		this.updatedBy = updatedBy;
 	}
-
 	public Date getUpdatedDate() {
 		return updatedDate;
 	}
-
 	public void setUpdatedDate(Date updatedDate) {
 		this.updatedDate = updatedDate;
 	}
-
 	public String getCreatedBy() {
 		return createdBy;
 	}
-
 	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
 	}
-
 	public Date getCreatedDate() {
 		return createdDate;
 	}
-
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
